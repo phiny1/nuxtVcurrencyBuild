5185c25d67189bf019810a8d51f45307d95f758c
Raphael Kammer <josekammer1@gmail.com>
2020-01-13 14:05:32 -0300

    Testing jsonschema with transpile

--- basicNuxt/nuxt.config.js
+++ basicNuxt/nuxt.config.js
@@ -1,78 +1,75 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: "%s - " + process.env.npm_package_name,
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      },
-      {
-        rel: 'stylesheet',
-        type: 'image/x-icon',
-        href: 'https://fonts.googleapis.com/css?family=Material+Icons'
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css'
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    script:[
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-        type: 'text/javascript'
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+      },
+      {
+        rel: "stylesheet",
+        type: "image/x-icon",
+        href: "https://fonts.googleapis.com/css?family=Material+Icons"
+      },
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdn.materialdesignicons.com/3.6.95/css/materialdesignicons.min.css"
+      }
+    ],
+    script: [
+      {
+        src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        type: "text/javascript"
       }
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/social.js',
-    '~/plugins/Vuetify.js',
-    '~/plugins/currency.js'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["~/plugins/social.js", "~/plugins/vCurrencyField.js"],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxtjs/vuetify',
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxtjs/vuetify"],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
+    ["v-currency-field/nuxt-treeshaking", { locale: "pt-BR", decimalLength: 2 }]
   ],
   /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
   vuetify: {
-    treeShake: true,
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -89,13 +86,13 @@ export default {
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
+    transpile: ["@koumoul/vuetify-jsonschema-form/lib/index.vue"],
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
--- basicNuxt/pages/inspire.vue
+++ basicNuxt/pages/inspire.vue
@@ -1,11 +1,7 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
+  <VRow>
+    <VCol class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
@@ -14,6 +10,113 @@
           </small>
         </footer>
       </blockquote>
-    </v-flex>
-  </v-layout>
+      <v-form v-model="formValid">
+        <VJsonschemaForm
+          v-if="schema"
+          :schema="schema"
+          :model="dataObject"
+          :options="options"
+          @error="showError"
+          @change="showChange"
+          @input="showInput"
+        />
+      </v-form>
+    </VCol>
+  </VRow>
 </template>
+
+<script>
+import VJsonschemaForm from "@koumoul/vuetify-jsonschema-form";
+
+export default {
+  components: { VJsonschemaForm },
+  data() {
+    return {
+      schema: {
+        $id: "https://example.com/person.schema.json",
+        $schema: "http://json-schema.org/draft-07/schema#",
+        title: "Person",
+        description:
+          "A few metadata about some person. Rendered as a form by [vuetify-jsonschema-form](https://github.com/koumoul-dev/vuetify-jsonschema-form).",
+        type: "object",
+        required: ["firstName", "lastName"],
+        properties: {
+          type: {
+            type: "string",
+            const: "person"
+          },
+          firstName: {
+            type: "string",
+            description:
+              "\nThe person's first name.\n\nThis description can be a long text with markdown content.\n\n  - a list item\n  - another one\n  ",
+            "x-class": "sm6 pr-4"
+          },
+          lastName: {
+            type: "string",
+            description: "The person's last name.",
+            "x-class": "sm6"
+          },
+          password: {
+            type: "string",
+            "x-display": "password"
+          },
+          age: {
+            description:
+              "Age in years which must be equal to or greater than zero.",
+            type: "integer",
+            minimum: 0,
+            maximum: 150
+          },
+          ageSlider: {
+            description: "Same age, but in a slider.",
+            type: "integer",
+            "x-display": "slider",
+            minimum: 0,
+            maximum: 150
+          },
+          internalKey: {
+            description:
+              "A property managed only internally by programs and hidden from user",
+            type: "string",
+            "x-display": "hidden"
+          },
+          citizen: {
+            description: "Is this person a citizen of this country.",
+            type: "boolean"
+          },
+          description: {
+            description: "A longer text for the description.",
+            type: "string",
+            maxLength: 2000
+          },
+          homepage: {
+            description:
+              "A long string also, but display is forced on single line",
+            type: "string",
+            maxLength: 2000,
+            "x-display": "single-line"
+          }
+        }
+      },
+      dataObject: {},
+      formValid: false,
+      options: {
+        debug: false,
+        disableAll: false,
+        autoFoldObjects: true
+      }
+    };
+  },
+  methods: {
+    showError(err) {
+      window.alert(err);
+    },
+    showChange(e) {
+      console.log('"change" event', e);
+    },
+    showInput(e) {
+      console.log('"input" event', e);
+    }
+  }
+};
+</script>
